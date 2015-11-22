455821675f493e2c61cec6a3da9c1e6e9e733716
tibus29 <thibault.pensec@gmail.com>
2015-11-22 21:30:19 +0100

    amélioration carousel

--- index.php
+++ index.php
@@ -1,46 +1,59 @@
 <?php
 if(is_front_page()) {
-    query_posts(array('category_name' => 'actualites', 'posts_per_page' => -1));
+
+    $postList = get_posts(array('category_name' => 'actualites'));
+
+    global $fb_tags;
+
+    $fb_tags = "<meta property=\"og:title\" content=\"".get_bloginfo('name')." - ".get_the_title()."\" />
+                <meta property=\"og:description\" content=\"".get_the_excerpt()."\" />
+                <meta property=\"og:url\" content=\"".get_home_url()."\" />
+                <meta property=\"og:image\" content=\"".wp_get_attachment_url( get_post_thumbnail_id() )."\" />";
+
 ?>
-    <div id="carousel-news" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <!--
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-        -->
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
+    <article class="home">
 
-            <?php $i = 0; while (have_posts()) : the_post(); ?>
-            <div class="item <?php echo $i++ == 0 ? 'active' : '' ?>">
-                <?php the_post_thumbnail(); ?>
-                <div class="carousel-caption">
-                    <h3><?php the_title(); ?></h3>
-                    <p>
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>" class="more">lire la suite...</a>
-                    </p>
-                </div>
+        <div id="carousel-news" class="carousel slide" data-ride="carousel">
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+
+                <?php $i = 0; foreach ( $postList as $post ) : setup_postdata( $post ); ?>
+                    <div class="item <?php echo $i++ == 0 ? 'active' : '' ?>">
+                        <?php the_post_thumbnail(); ?>
+                        <div class="carousel-caption">
+                            <h3><?php the_title(); ?></h3>
+                            <p>
+                                <?php the_excerpt(); ?>
+                                <a href="<?php the_permalink(); ?>" class="more">lire la suite...</a>
+                            </p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
-            <?php endwhile; ?>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-news" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-news" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+            <?php $i = 0; foreach ( $postList as $post ) : setup_postdata( $post ); ?>
+                <li data-target="#carousel-news" data-slide-to="<?php echo $i; ?>" class="<?php echo $i++ == 0 ? 'active' : '' ?>"></li>
+            <?php endforeach; ?>
+
         </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-news" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-news" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        <a class="large-btn" title="Voir tous les articles" href="<?php echo esc_url(get_category_link(get_cat_ID('actualites'))); ?>">voir tous les articles</a>
 
-    <a title="Voir tous les articles" href="<?php esc_url(get_category_link(get_cat_ID('actualites'))) ?>">voir tous les articles</a>
+    </article>
 
 <?php } else { ?>
 
--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,14 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php
+        global $fb_tags;
+        if (isset ($fb_tags)) {
+            echo $fb_tags;
+        }
+    ?>
+
+    <?php wp_head(); ?>
 </head>
