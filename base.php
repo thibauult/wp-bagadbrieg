2ca2688d7dbaa0e460f60b1c6364fcbd1d58f960
tibus29 <thibault.pensec@gmail.com>
2015-11-24 19:05:29 +0100

    clean scss

--- base.php
+++ base.php
@@ -19,16 +19,16 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/header');
     ?>
 
-    <div class="container" role="document">
+    <div class="base container" role="document">
       <div class="row">
 
-        <div class="col-md-3">
+        <div class="col-md-3 w-20">
           <aside><?php include Wrapper\sidebar_left_path(); ?></aside>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 w-60">
           <main><?php include Wrapper\template_path(); ?></main>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-3 w-20">
           <aside><?php include Wrapper\sidebar_right_path(); ?></aside>
         </div>
 
--- templates/header.php
+++ templates/header.php
@@ -2,13 +2,13 @@
     <header class="header">
 
       <div class="row">
-        <div class="col-md-3 text-center">
+        <div class="col-md-3 w-20 text-center">
             <a href="<?= esc_url(home_url('/')); ?>">
                 <img class="img-responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.png" />
             </a>
         </div>
 
-        <div class="col-md-6 text-center">
+        <div class="col-md-6 w-60 text-center">
             <div class="brand-wrapper">
                 <h1>Bagad <strong>Brieg</strong> Pipe Band</h1>
             </div>
