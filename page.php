9c1995c9ade3ed653058694ec164633782a470fd
tibus29 <thibault.pensec@gmail.com>
2015-11-28 00:32:40 +0100

    center video in page

--- page.php
+++ page.php
@@ -5,3 +5,9 @@ while (have_posts()) : the_post();
     get_template_part('templates/page', 'footer');
     comments_template('/templates/comments.php');
 endwhile;
+?>
+<script type="text/javascript">
+    (function($) {
+        $("article > p:has(> iframe)").addClass('text-center'); // detect video and center it
+    })(jQuery);
+</script>
